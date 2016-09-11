04e98e1a31475c76571112421959ed8cb0a9765f
Vaibhav Bali <vibsbali@gmail.com>
2016-09-12 01:22:31 +1000

    Added controller and html for Edit functionality

---
+++ FrontToBack.Client/app/products/productEditController.js
@@ -0,0 +1,32 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("productManagement").controller("ProductEditCtrl", function(productResource) {
+
+        var vm = this;
+            vm.product = {};
+            vm.message = "";
+
+        //using $resource
+            productResource.get({ id: 5 },
+                function(data) {
+                    vm.product = data;
+                    vm.originalProduct = angular.copy(data);
+                });
+
+            if (vm.product && vm.product.productId) {
+                vm.title = "Edit: " + vm.product.productName;
+            } else {
+                vm.title = "New Product";
+            }
+
+            vm.submit = function() {
+            };
+
+            vm.cancel = function(editForm) {
+                editForm.$setPristine();
+                vm.product = angular.copy(vm.originalProduct);
+                vm.message = "";
+            }
+        });
+}());
