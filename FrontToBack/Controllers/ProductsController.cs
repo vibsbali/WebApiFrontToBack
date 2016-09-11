a0a175ffb227a4ef4c441546ccdf6c294fe0a028
Vaibhav Bali <vibsbali@gmail.com>
2016-09-11 17:32:53 +1000

    Created a Products Controller

---
+++ FrontToBack/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using FrontToBack.Models;
+using System.Web.Http;
+
+namespace FrontToBack.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private ProductRepository repository = new ProductRepository();
+
+        // GET: api/Products
+        public IHttpActionResult Get()
+        {
+            return Ok(repository.Retrieve());
+        }
+
+        // GET: api/Products/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Products
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Products/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Products/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
