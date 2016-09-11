98f32e5d21df2402d61b41dd3a201e2d35978d21
Vaibhav Bali <vibsbali@gmail.com>
2016-09-11 17:28:00 +1000

    Added repository

---
+++ FrontToBack/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FrontToBack.Models
+{
+    public class Product
+    {
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ProductCode { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
