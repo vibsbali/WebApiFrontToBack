56e454e74a513b433129bd3cd001d608971363b4
Vaibhav Bali <vibsbali@gmail.com>
2016-09-11 17:59:22 +1000

    Created resource service in Angular

---
+++ FrontToBack.Client/app/common/common.services.js
@@ -0,0 +1,8 @@
+﻿"use strict";
+
+(function() {
+    angular.module("common.services", ["ngResource"])
+        .constant("appSettings", {
+            serverPath: "http://localhost:8922"
+        });
+}())
---
+++ FrontToBack.Client/app/common/productResource.js
@@ -0,0 +1,11 @@
+﻿"use strict";
+
+(function() {
+    angular.module("common.services").factory("productResource", [
+        "$resource", "appSettings", function ($resource, appSettings) {
+            //returns $resource object
+            //The parameters are url with id as optional
+            return $resource(appSettings.serverPath + "/api/products/:id");
+        }
+    ]);
+}())
