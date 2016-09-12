653c7b9bef158921fa061464a0526c1fa7de80d6
Vaibhav Bali <vibsbali@gmail.com>
2016-09-12 15:02:46 +1000

    Added login code

--- FrontToBack.Client/app/common/userAccount.js
+++ FrontToBack.Client/app/common/userAccount.js
@@ -6,10 +6,36 @@
 
             //returns $resource object
             //We are customising $resource to create a new registerUser method
-            return $resource(appSettings.serverPath + "/api/Account/Register", null,
+            //return $resource(appSettings.serverPath + "/api/Account/Register", null,
+            //    {
+            //        "registerUser": { method: "POST" }
+            //    });
+
+            //A more complex custom userAccount Service that exposes two custom Post methods for different Url addresses
+            return{
+                registration: $resource(appSettings.serverPath + "/api/Account/Register", null,
                 {
-                    "registerUser": { method: "POST" }
-                });
+                    "registerUser": {
+                        method: "POST"
+                    }
+                }),
+                login: $resource(appSettings.serverPath + "/Token", null,
+                {
+                    "loginUser" : {
+                        method: "POST",
+                        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+                        //following method will encode the properties to form-urlendoded as required by our Web Api
+                        //If we do not follow following step then the values will be posted as json which will not work with our web api
+                        transformRequest: function(data, headersGetter) {
+                            var str = [];
+                            for (var d in data) {
+                                str.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
+                            }
+                            return str.join("&");
+                        }
+                    }
+                })
+            }
         }
     ]);
 }())
--- FrontToBack.Client/app/mainController.js
+++ FrontToBack.Client/app/mainController.js
@@ -17,10 +17,12 @@
                 vm.registerUser = function () {
                     vm.userData.confirmPassword = vm.userData.password;
 
-                    userAccount.registerUser(vm.userData, function (data) {
+                    userAccount.registration.registerUser(vm.userData, function (data) {
                             console.log(data);
                         vm.confirmPassword = "";
                         vm.message = ".... Registration successful";
+
+                        //once registered call login method
                         vm.login();
                     }, function (response) {
                         console.log(response);
@@ -32,15 +34,35 @@
                                 vm.message += response.data.modelState[key] + "\r\n";
                             }
                         }
-                    }
-                    );
+                    });
                 };
 
                 vm.login = function () {
+                    vm.userData.grant_type = "password";
+                    vm.userData.userName = vm.userData.email;
 
+                    userAccount.login.loginUser(vm.userData,
+                        function(data) {
+                            vm.isLoggedIn = true;
+                            vm.message = "";
+                            vm.password = "";
+                            vm.token = data.access_token;
+                        },
+                        function(response) {
+                            vm.password = "";
+                            vm.isLoggedIn = false;
+                            vm.message = response.statusText + "\r\n";
+
+                            //Following code handles cases when exception gets thrown from the web api
+                            if (response.data.exceptionMessage) {
+                                vm.message += response.data.exceptionMessage;
+                            }
+
+                            if (response.data.error) {
+                                vm.message += response.data.error;
+                            }
+                        });
                 };
-
-
             }
         ]);
 }());
